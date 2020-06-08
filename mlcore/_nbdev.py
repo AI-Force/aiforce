761b5bed308bc64d40707d7a233879922e12d43b
Sebastian Rettig <s-rettig@protosolution.co.jp>
2020-06-08 15:24:16 +0900

    added dataset generator

--- mlcore/_nbdev.py
+++ mlcore/_nbdev.py
@@ -2,9 +2,39 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"CATEGORY_LABEL_KEY": "dataset.ipynb",
+         "IMAGE_SET_FOLDER": "dataset.ipynb",
+         "DEFAULT_CATEGORIES_FILE": "dataset.ipynb",
+         "DEFAULT_CLASSIFICATION_ANNOTATIONS_FILE": "dataset.ipynb",
+         "DEFAULT_SEGMENTATION_ANNOTATIONS_FILE": "dataset.ipynb",
+         "DEFAULT_SPLIT": "dataset.ipynb",
+         "DATA_SET_FOLDER": "dataset.ipynb",
+         "SEMANTIC_MASK_FOLDER": "dataset.ipynb",
+         "IMAGE_EXTENSIONS": "dataset.ipynb",
+         "FOLDER_FILTER": "dataset.ipynb",
+         "IMAGES_TRAIN_VAL_FOLDER": "dataset.ipynb",
+         "IMAGES_TEST_FOLDER": "dataset.ipynb",
+         "DATA_SET_TRAIN_FOLDER": "dataset.ipynb",
+         "DATA_SET_VAL_FOLDER": "dataset.ipynb",
+         "DATA_SET_TEST_FOLDER": "dataset.ipynb",
+         "IMAGE_SEGMENTATION": "dataset.ipynb",
+         "IMAGE_CLASSIFICATION": "dataset.ipynb",
+         "NOT_CATEGORIZED": "dataset.ipynb",
+         "logger": "dataset.ipynb",
+         "read_categories": "dataset.ipynb",
+         "DataSet": "dataset.ipynb",
+         "ClassificationDataSet": "dataset.ipynb",
+         "SegmentationDataSet": "dataset.ipynb",
+         "scan_files": "dataset.ipynb",
+         "scan_path": "dataset.ipynb",
+         "delete_folder": "dataset.ipynb",
+         "create_folder": "dataset.ipynb",
+         "split_train_val_data": "dataset.ipynb",
+         "input_feedback": "dataset.ipynb",
+         "configure_logging": "dataset.ipynb",
+         "build_data_set": "dataset.ipynb"}
 
-modules = []
+modules = ["dataset.py"]
 
 doc_url = "https://s-rettig.github.io/mlcore/"
 
